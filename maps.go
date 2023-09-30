0bdb79768432b6a9ab700e6568dba13970bf7ef6
ShatilKhan <shatilshahriar009@gmail.com>
2023-09-30 21:53:02 +0600

    update maps

--- maps.go
+++ maps.go
@@ -27,4 +27,13 @@ func main(){
 	fmt.Println("prs:", prs)
 	
 
+	n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
+
+
+    n2 := map[string]int{"foo": 1, "bar": 2}
+    if maps.Equal(n, n2) {
+        fmt.Println("n == n2")
+    }
+
 }
